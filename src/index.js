ceee551d36f0964a557e1ea854407c0ff4e12eff
Aayush Seth <aayushseth8@gmail.com>
2021-08-02 00:15:07 -0700

    Test out Project and TodoItems work

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 // Will use this module to set everything up
 // Import all other modules into here to inialize and coordinate everything
+
+import Project from "./project";
+import TodoItem from "./todoItem";
+
+const defaultProject = Project();
+
+defaultProject.addTodo(TodoItem("Default", "...", "01/01/2021", "Medium"));
+
+console.log(defaultProject.getTodos());
