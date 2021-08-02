bac94020e51c1b025a086dda07401bdf6997b45b
Aayush Seth <aayushseth8@gmail.com>
2021-08-02 00:14:40 -0700

    Set up factory function for project

--- src/project.js
+++ src/project.js
@@ -1,1 +1,18 @@
-// Module representing a list of todo items
+// Module representing a project
+
+// Each project has a list to hold all todo items
+
+import TodoItem from "./todoItem";
+
+const Project = () => {
+    const todos = [];
+
+    let getTodos = () => todos;
+    let addTodo = (todoItem) => {
+        todos.push(todoItem);
+    }
+
+    return { getTodos, addTodo }
+};
+
+export default Project;
