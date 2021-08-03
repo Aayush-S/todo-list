d651831bd1551aeb33334d82f747722fd014e716
Aayush Seth <aayushseth8@gmail.com>
2021-08-02 17:59:35 -0700

    Create UI to handle DOM related logic

--- src/UI.js
+++ src/UI.js
@@ -1,1 +1,37 @@
 // Renders everything with the DOM
+
+function renderTodoItem(todoItem) {
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo-item');
+    todoDiv.classList.add(todoItem.getPriority());
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList = "status-check";
+    todoDiv.append(checkbox); 
+    
+    const title = document.createElement('h1');
+    title.textContent = todoItem.getTitle();
+    todoDiv.append(title);
+    
+    const dueDate = document.createElement('p');
+    dueDate.textContent = todoItem.getDueDate();
+    todoDiv.append(dueDate);
+
+    return todoDiv;
+}
+
+function renderProject(project) {
+    const projectDiv = document.createElement('div');
+    projectDiv.classList.add('project');
+
+    const title = document.createElement('h1');
+    title.textContent = project.getTitle();
+    projectDiv.append(title);
+
+    project.getTodos().forEach(todo => projectDiv.append(renderTodoItem(todo)));
+
+    return projectDiv;
+}
+
+export { renderProject };
