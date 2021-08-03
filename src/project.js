6ad014a9f27c82cbdae1801822939faf1ec9a998
Aayush Seth <aayushseth8@gmail.com>
2021-08-02 18:00:15 -0700

    Add title property to each Project

--- src/project.js
+++ src/project.js
@@ -4,15 +4,21 @@
 
 import TodoItem from "./todoItem";
 
-const Project = () => {
+const Project = (title) => {
     const todos = [];
+    const _title = title; 
 
     let getTodos = () => todos;
     let addTodo = (todoItem) => {
         todos.push(todoItem);
     }
 
-    return { getTodos, addTodo }
+    let getTitle = () => _title;
+    let setTitle = (newTitle) => {
+        _title = newTitle;
+    }
+
+    return { getTodos, addTodo, getTitle, setTitle }
 };
 
 export default Project;
