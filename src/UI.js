ff9eb6841d173267c74d40e0076bda4a6aab2e8b
Aayush Seth <aayushseth8@gmail.com>
2021-08-03 18:38:48 -0700

    Refactor to separate creating and rendering nodes

--- src/UI.js
+++ src/UI.js
@@ -1,4 +1,6 @@
 // Renders everything with the DOM
+import TodoItem from "./todoItem";
+import Project from "./project";
 
 function renderTodoItem(todoItem) {
     const todoDiv = document.createElement('div');
@@ -21,7 +23,7 @@ function renderTodoItem(todoItem) {
     return todoDiv;
 }
 
-function renderProject(project) {
+function createProjectNode(project) {
     const projectDiv = document.createElement('div');
     projectDiv.classList.add('project');
 
@@ -31,7 +33,65 @@ function renderProject(project) {
 
     project.getTodos().forEach(todo => projectDiv.append(renderTodoItem(todo)));
 
+    const form = renderForm(project);
+
+    projectDiv.append(form);
+
     return projectDiv;
 }
 
-export { renderProject };
+function renderProject(project) {
+
+    const projectNode = createProjectNode(project);
+    const projectContainer = document.querySelector('.project-container');
+
+    // empty the container
+    while (projectContainer.firstChild) {
+        projectContainer.removeChild(projectContainer.firstChild)
+    }
+
+    // add the project node into the container
+    projectContainer.append(projectNode);
+}
+
+function renderForm(project) {
+    const formDiv = document.createElement('div');
+    formDiv.classList.add('form');
+
+    const textbox = document.createElement('input');
+    textbox.type = "text";
+    textbox.classList.add('form-textbox');
+    formDiv.append(textbox);
+
+    const submitBtn = document.createElement('button');
+    submitBtn.textContent = "Create";
+    submitBtn.classList.add('submit-btn');
+
+    submitBtn.addEventListener('click', () => {
+        project.addTodo(TodoItem(textbox.textContent, "desc", "dueDate", "priority"));
+        // render all the todos again.
+    });
+    formDiv.append(submitBtn);
+
+    return formDiv;
+}
+
+function initHomePage() {
+    // sets up the html skeleton for the homescreen
+    const content = document.querySelector('#content');
+
+    const header = document.createElement('div');
+    header.classList.add('header');
+    content.append(header);
+
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('project-container');
+    content.append(projectContainer);
+
+    const projectsListContainer = document.createElement('div');
+    projectsListContainer.classList.add('projects-list-container');
+    content.append(projectsListContainer);
+
+}
+
+export { initHomePage, renderProject };
