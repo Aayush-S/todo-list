eaec05b9148df75791efd9bd925ad84ad29c36e9
Aayush Seth <aayushseth8@gmail.com>
2021-08-05 00:35:54 -0700

    Change properties and params

--- src/todoItem.js
+++ src/todoItem.js
@@ -3,12 +3,12 @@
 // Will include:
 // title, description, dueDate and priority. Optional: notes and/or checklist.
 
-const TodoItem = (_title, _desc, _dueDate, _priority) => {
+const TodoItem = (_title, _desc, _dueDate, _priority, _status) => {
     let title = _title;
     let description = _desc;
     let dueDate = _dueDate;
     let priority = _priority;
-    let status = false;
+    let status = _status;
     
     return { title, description, priority, dueDate, status }
 }
